7bf0dca30739863abdc30f541e98fa4e89b87e79
joebalanoff <64605956+joebalanoff@users.noreply.github.com>
2020-07-18 00:27:31 -0500

    Fixed VS

---
+++ MultitoolTheGame/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed);
+    }
+}
